4d237a9437276e24351b1530b9716300d1bab213
Home <jamesdordoy@gmail.com>
2018-08-02 22:11:17 +0100

    Removed useless code.

--- public/js/app.js
+++ public/js/app.js
@@ -64246,11 +64246,8 @@ module.exports = Component.exports
 
 /***/ }),
 /* 218 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports) {
 
-"use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__objects_SteeringBehavior__ = __webpack_require__(239);
 //
 //
 //
@@ -64258,40 +64255,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
-
-
-
-
-/* harmony default export */ __webpack_exports__["default"] = ({
-    created: function created() {
-        var str = void 0;
-        var width = void 0;
-
-        if (typeof window.orientation !== 'undefined') {
-            str = 'JD';
-            width = 300;
-        } else {
-            str = 'Coming Soon';
-            width = 1060;
-        }
-
-        // P5 JS Libary
-        var steeringBehavior = new __WEBPACK_IMPORTED_MODULE_0__objects_SteeringBehavior__["a" /* default */](str, width);
-
-        function preload() {
-            steeringBehavior.preload();
-        }
-
-        function setup() {
-            steeringBehavior.setup();
-        }
-
-        function draw() {
-            steeringBehavior.draw();
-        }
-    }
-
-});
 
 /***/ }),
 /* 219 */
@@ -64318,142 +64281,6 @@ if (false) {
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
-
-/***/ }),
-/* 221 */,
-/* 222 */,
-/* 223 */,
-/* 224 */,
-/* 225 */,
-/* 226 */,
-/* 227 */,
-/* 228 */,
-/* 229 */,
-/* 230 */,
-/* 231 */,
-/* 232 */,
-/* 233 */,
-/* 234 */,
-/* 235 */,
-/* 236 */,
-/* 237 */,
-/* 238 */,
-/* 239 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Vehicle_js__ = __webpack_require__(240);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__Vehicle_js___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__Vehicle_js__);
-
-
-function SteeringBehavior(name) {
-
-  this.font = null;
-  this.vehicles = [];
-  this.name = name;
-
-  this.preload = function () {
-    this.font = loadFont('/fonts/AvenirNextLTPro-Demi.otf');
-  };
-
-  this.setup = function () {
-    var canvas = createCanvas(width, 300);
-    background('#36454f');
-    canvas.parent('name');
-
-    var points = steeringBehavior.font.textToPoints(this.name, 100, 200, 160, {
-      sampleFactor: 0.25
-    });
-
-    for (var i = 0; i < points.length; i++) {
-      var pt = points[i];
-      var vehicle = new __WEBPACK_IMPORTED_MODULE_0__Vehicle_js___default.a(pt.x, pt.y);
-      this.vehicles.push(vehicle);
-    }
-  };
-
-  this.draw = function () {
-
-    background('#36454f');
-    for (var i = 0; i < this.vehicles.length; i++) {
-      var v = this.vehicles[i];
-      v.behaviors();
-      v.update();
-      v.show();
-    }
-  };
-}
-
-/* harmony default export */ __webpack_exports__["a"] = (SteeringBehavior);
-
-/***/ }),
-/* 240 */
-/***/ (function(module, exports) {
-
-function Vehicle(x, y) {
-  this.pos = createVector(random(width), random(height));
-  this.target = createVector(x, y);
-  this.vel = p5.Vector.random2D();
-  this.acc = createVector();
-  this.r = 8;
-  this.maxspeed = 10;
-  this.maxforce = 1;
-}
-
-Vehicle.prototype.behaviors = function () {
-  var arrive = this.arrive(this.target);
-  var mouse = createVector(mouseX, mouseY);
-  var flee = this.flee(mouse);
-
-  arrive.mult(1);
-  flee.mult(5);
-
-  this.applyForce(arrive);
-  this.applyForce(flee);
-};
-
-Vehicle.prototype.applyForce = function (f) {
-  this.acc.add(f);
-};
-
-Vehicle.prototype.update = function () {
-  this.pos.add(this.vel);
-  this.vel.add(this.acc);
-  this.acc.mult(0);
-};
-
-Vehicle.prototype.show = function () {
-  stroke(255);
-  strokeWeight(this.r);
-  point(this.pos.x - 70, this.pos.y);
-};
-
-Vehicle.prototype.arrive = function (target) {
-  var desired = p5.Vector.sub(target, this.pos);
-  var d = desired.mag();
-  var speed = this.maxspeed;
-  if (d < 100) {
-    speed = map(d, 0, 100, 0, this.maxspeed);
-  }
-  desired.setMag(speed);
-  var steer = p5.Vector.sub(desired, this.vel);
-  steer.limit(this.maxforce);
-  return steer;
-};
-
-Vehicle.prototype.flee = function (target) {
-  var desired = p5.Vector.sub(target, this.pos);
-  var d = desired.mag();
-  if (d < 50) {
-    desired.setMag(this.maxspeed);
-    desired.mult(-1);
-    var steer = p5.Vector.sub(desired, this.vel);
-    steer.limit(this.maxforce);
-    return steer;
-  } else {
-    return createVector(0, 0);
-  }
-};
 
 /***/ })
 /******/ ]);
