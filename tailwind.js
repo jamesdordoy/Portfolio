a75a596156356cf178a50e191b168188e8a0684c
Dordoy <jamesdordoy@gmail.com>
2020-09-02 14:34:48 +0100

    Tidying up

--- tailwind.js
+++ tailwind.js
@@ -10,4 +10,9 @@ module.exports = {
     future: {
         removeDeprecatedGapUtilities: true,
     },
+    purge: [
+        './src/**/*.html',
+        './src/**/*.vue',
+        './src/**/*.jsx',
+    ],
 }
