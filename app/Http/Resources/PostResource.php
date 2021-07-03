a671505289c49fcc5d49f0992a16a74c5f1542da
James Dordoy <jamesdordoy@users.noreply.github.com>
2021-07-03 18:47:42 +0000

    Apply fixes from StyleCI

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Resources;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 class PostResource extends JsonResource
 {
