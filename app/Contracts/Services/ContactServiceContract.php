528cb19f6491312ba2c8dd5f2677f5c140ff0922
James Dordoy <jamesdordoy@users.noreply.github.com>
2021-08-29 16:46:19 +0000

    Apply fixes from StyleCI

--- app/Contracts/Services/ContactServiceContract.php
+++ app/Contracts/Services/ContactServiceContract.php
@@ -7,15 +7,15 @@ use App\Models\Newsletter;
 
 interface ContactServiceContract
 {
-    public function store(array $data) : Contact;
+    public function store(array $data): Contact;
 
-    public function storeNewsletter(string $email) : Newsletter;
+    public function storeNewsletter(string $email): Newsletter;
 
-    public function unsubscribeFromNewsletter(Newsletter $newsletter) : bool;
+    public function unsubscribeFromNewsletter(Newsletter $newsletter): bool;
 
-    public function sendContactMeEmail(Contact $contact) : void;
+    public function sendContactMeEmail(Contact $contact): void;
 
-    public function sendLetMeKnowEmail(Contact $contact) : void;
+    public function sendLetMeKnowEmail(Contact $contact): void;
 
-    public function sendNewsLetterEmail(Newsletter $newsletter) : void;
+    public function sendNewsLetterEmail(Newsletter $newsletter): void;
 }
