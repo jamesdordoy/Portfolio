16bb961cd6bb53201c643fcaabe915f84003bcd4
James Dordoy <jamesdordoy@users.noreply.github.com>
2021-08-29 14:50:10 +0000

    Apply fixes from StyleCI

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Telescope\IncomingEntry;
 use Laravel\Telescope\Telescope;
 use Laravel\Telescope\TelescopeApplicationServiceProvider;
 
