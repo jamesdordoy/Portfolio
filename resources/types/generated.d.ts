a600b2d6b2fc28eeabafd3b3b372196c8fe38e07
James Dordoy <19800113+jamesdordoy@users.noreply.github.com>
2023-01-23 20:01:14 +0000

    Tidying

--- resources/types/generated.d.ts
+++ resources/types/generated.d.ts
@@ -1,45 +1,52 @@
 declare namespace App.Models.Data {
     export type Contact = {
-        id: number
-        name: string
-        email: string
-        message: string
-        created_at: string
-        updated_at: string
-    }
+        id: number;
+        name: string;
+        email: string;
+        message: string;
+        created_at?: string;
+        updated_at?: string;
+    };
+    export type Language = {
+        id?: number;
+        name: string;
+        description: string;
+        created_at?: string;
+        updated_at?: string;
+    };
     export type Project = {
-        id: number
-        name: string
-        description: string
-        owner: string
-        link: string
-        icon: string
-        completed: boolean
-        private: boolean
-        created_at: string
-        updated_at: string
-        tags: Array<App.Models.Data.Tag>
-    }
+        id: number;
+        name: string;
+        description: string;
+        owner: string;
+        link: string;
+        icon: string;
+        completed: boolean;
+        private: boolean;
+        created_at: string;
+        updated_at: string;
+        tags: Array<App.Models.Data.Tag>;
+    };
     export type Tag = {
-        id: number
-        taggable_id: string
-        taggable_type: string
-    }
+        id: number;
+        taggable_id: string;
+        taggable_type: string;
+    };
     export type TimelineEvent = {
-        id: number
-        from: string
-        to: string
-        name: string
-        title: string
-        icon: string
-        body: string
-        hidden: boolean
-        created_at: string
-        updated_at: string
-    }
+        id: number;
+        from: string;
+        to: string;
+        name: string;
+        title: string;
+        icon: string;
+        body: string;
+        hidden: boolean;
+        created_at: string;
+        updated_at: string;
+    };
     export type User = {
-        id: number
-        name: string
-        email: string
-    }
+        id: number;
+        name: string;
+        email: string;
+    };
 }
