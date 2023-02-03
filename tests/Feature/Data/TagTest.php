12259aabdb6b28dab7af6aabf01324c37ff9f9db
jamesdordoy <jamesdordoy@users.noreply.github.com>
2023-02-03 20:12:29 +0000

    PHP Linting (Pint)

--- tests/Feature/Data/TagTest.php
+++ tests/Feature/Data/TagTest.php
@@ -5,7 +5,6 @@ use App\Models\Data\Tag;
 use App\Models\Data\Taggable;
 
 test('test_language_data_object_name', function () {
-
     $taggable = new Taggable(1, 'php', 'php');
     $tag = new Tag(1, 1, App\Models\Language::class, $taggable);
 
