18f13ab37386797106ff6e9fc25d7d6e51d52dbc
James Dordoy <me@jamesdordoy.co.uk>
2026-06-15 19:01:19 +0100

    Update inertia.php

--- config/inertia.php
+++ config/inertia.php
@@ -6,12 +6,12 @@ return [
         'url' => 'http://127.0.0.1:13714',
         // 'bundle' => base_path('bootstrap/ssr/ssr.mjs'),
     ],
-    'testing' => [
-        'ensure_pages_exist' => true,
-        'page_paths' => [
+    'pages' => [
+        'ensure_pages_exist' => false,
+        'paths' => [
             resource_path('js/Pages'),
         ],
-        'page_extensions' => [
+        'extensions' => [
             'js',
             'jsx',
             'svelte',
@@ -19,5 +19,8 @@ return [
             'tsx',
             'vue',
         ],
+    ],
+    'testing' => [
+        'ensure_pages_exist' => true,
     ],
 ];
